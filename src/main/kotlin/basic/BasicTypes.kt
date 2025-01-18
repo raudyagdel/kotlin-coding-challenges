ce486d75ca138d9cd792cbc0aa86a5bcf4fd07cd
ASMR SCRIPT <raudyagdel@gmail.com>
2025-01-18 00:23:52 -0300

    - Warming Up

---
+++ src/main/kotlin/basic/BasicTypes.kt
@@ -0,0 +1,69 @@
+package basic
+
+fun main() {
+//    var number = 10
+//    println(number) // Print 10
+
+//    number = 8
+//    println(number) // Print 8
+
+// Operators (+, -, *, /, %)
+//    number = 100
+
+//    number += 10 // 110
+//    number -= 10 // 90
+//    number *= 3  // 300
+//    number /= 5  // 20
+//    number %= 3  // 1
+
+// Integer Data Types (Byte, Short, Int, Long)
+//    val b : Byte = 127 // From -127 To 127
+//    println(b)
+//
+//    val s: Short =  32767 // From -32767 To 32767
+//    println(s)
+//
+//    val i: Int =  2147483647  // From -2147483648 To 2147483648
+//    println(i)
+//
+//    val l: Long = 9223372036854775807 // From -9223372036854775807 to 9223372036854775807
+//    println(l)
+
+// Unsigned Integer Same But Without Signed Bit
+//    val ub: UByte = 255u
+//    val us: UShort = 65535u
+//    val ui: UInt = 4294967295u
+//    val ul: ULong = 18446744073709551615u
+
+// Floating Data Types (Float, Double)
+//    val f: Float = 3.14f
+//    val p: Double = 3.14
+//    println("$f - $p")
+
+// Booleans
+//    val isEnabled: Boolean = true
+//    val isDisabled: Boolean = false
+
+// Characters & Strings
+//    val c: Char = 'c'
+//    println(c)
+//
+//    val hi : String = "Hello, World!"
+//    println(hi)
+
+//    Explicitly declare the correct type for each variable:
+//    val a: Int = 1000
+//    val b = "log message"
+//    val c = 3.14
+//    val d = 100_000_000_000_000
+//    val e = false
+//    val f = '\n'
+
+    val a: Short = 1000
+    val b: String = "log message"
+    val c: Double= 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+
+}
---
+++ src/main/kotlin/basic/HelloWorld.kt
@@ -0,0 +1,15 @@
+package basic
+
+fun main() {
+    val m = "World"
+    println("Hello, $m!")
+
+    // Exercise
+    //Complete the code to make the program
+    // print "Mary is 20 years old" to standard output:
+
+    val name = "Mary"
+    val age = 20
+    println("$name is $age years old")
+
+}
